2b736465a17d11806a28bb07eacd9f41895cd6a7
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-07-18 01:09:29 +0100

    update

--- Coursetro.sol
+++ Coursetro.sol
@@ -4,10 +4,16 @@ contract Coursetro {
     
    string fName;
    uint age;
+   
+    event Instructor(
+       string name,
+       uint age
+    );
 
    function setInstructor(string _fName, uint _age) public {
        fName = _fName;
        age = _age;
+       Instructor(_fName, _age);
    }
    
    function getInstructor() view public returns (string, uint) {
