15179a06c55fc6ca41abdba4b6c0add6d703131d
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-07-18 01:12:48 +0100

    update 2

--- Coursetro.sol
+++ Coursetro.sol
@@ -4,13 +4,23 @@ contract Coursetro {
     
    string fName;
    uint age;
+   address owner;
+   
+   function Coursetro() public {
+       owner = msg.sender;
+   }
+   
+   modifier onlyOwner {
+       require(msg.sender == owner);
+       _;
+   }
    
     event Instructor(
        string name,
        uint age
     );
 
-   function setInstructor(string _fName, uint _age) public {
+   function setInstructor(string _fName, uint _age) onlyOwner public {
        fName = _fName;
        age = _age;
        Instructor(_fName, _age);
